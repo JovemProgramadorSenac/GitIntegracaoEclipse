bf6a74a32a3e5f57efc519c36a7a9ce26dadfdfc
JovemProgramadorSenac <JovemProgramadorSenac@gmail.com>
2024-07-03 14:40:22 -0300

    commit inicial para o github

---
+++ IntegracaoGitEclipse/src/gitTeste/TesteGitEclipse.java
@@ -0,0 +1,11 @@
+package gitTeste;
+
+public class TesteGitEclipse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Integração Git Eclipse Primeiro Teste.");
+
+	}
+
+}
