4da36d7f6164846f0c5fd939f3d400babbe841a6
JovemProgramadorSenac <jovemprogramadorsenac@gmail.com>
2024-07-03 15:09:02 -0300

    Update TesteGitEclipse.java

--- IntegracaoGitEclipse/src/gitTeste/TesteGitEclipse.java
+++ IntegracaoGitEclipse/src/gitTeste/TesteGitEclipse.java
@@ -5,6 +5,7 @@ public class TesteGitEclipse {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Integração Git Eclipse Primeiro Teste.");
+		System.out.println("Alterando do  Git  para o Eclipse.");
 
 	}
 
